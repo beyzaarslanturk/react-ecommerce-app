b28913639fc736b7ba38812373c5b977dbb80eca
Beyza Arslanturk <bnur.arslanturk@gmail.com>
2023-06-11 00:12:20 +0300

    second commit

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -24,6 +24,7 @@ export const Cart = () => {
           if (cartItems[product.id] !== 0) {
             return <CartItem data={product} />
           }
+          return null;
         })}
       </div>
       {totalAmount > 0 ?
